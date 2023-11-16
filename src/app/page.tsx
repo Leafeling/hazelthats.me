57744a6cdaca8ffb69097f9700cc8cfaac1e31e6
Hazel <hazel@theopensource.company>
2023-11-16 16:21:16 +0100

    No blog, no Twitter.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import styles from '@/styles/Home.module.scss'
 import { Metadata } from 'next';
-import BlogPost from './components/BlogPost';
 
 export const metadata: Metadata = {
     title: "Hazel, that's me.",
@@ -16,16 +15,6 @@ const Home = () =>
     <div className={styles.main}>
         <div className={styles.profile}>
             <h1><a href='./name'>Hazel</a>{", that's me."}</h1>
-            <span className={styles.sub}>The blog of one girl with too many interests</span>
-        </div>
-
-        <div className={styles.posts}>
-            <BlogPost
-                href="./blog/2023-09-17"
-                title="Starting a Blog"
-                description="I'm starting a weekly blog for whoever wants to read it."
-                date="September 17th, 2023"
-                />
         </div>
     </div>
 

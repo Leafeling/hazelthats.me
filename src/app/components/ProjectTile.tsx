eafbf71bb9aa30a08162d4fe3528898899259dfe
Hazel <hazel@theopensource.company>
2023-11-17 21:57:52 +0100

    Actually show off some stuff

---
+++ src/app/components/ProjectTile.tsx
@@ -0,0 +1,19 @@
+import styles from '@/styles/ProjectTile.module.scss';
+
+export function ProjectTile({ children, background, links }: {
+    children: React.ReactNode,
+    background: string,
+    links: React.ReactNode[]
+}) {
+    
+    return <div className={ styles.tile } style={{ background, backgroundSize: 'cover' }}>
+        <div className={ styles.content }>
+            <div className={ styles.inner }>
+                {children}
+            </div>
+            <div className={ styles.links }>
+                {links}
+            </div>
+        </div>
+    </div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import styles from '@/styles/Home.module.scss'
 import { Metadata } from 'next';
+import { ProjectTile } from './components/ProjectTile';
+import { SiGithub, SiMastodon } from '@icons-pack/react-simple-icons';
 
 export const metadata: Metadata = {
     title: "Hazel, that's me.",
@@ -13,8 +15,49 @@ export const metadata: Metadata = {
 
 const Home = () =>
     <div className={styles.main}>
-        <div className={styles.profile}>
-            <h1><a href='./name'>Hazel</a>{", that's me."}</h1>
+        <h1><a href='./name'>Hazel</a>{", that's me."}</h1>
+
+        <h2>Here's what I've worked on.</h2>
+
+        <div className={styles.showcase}>
+            <ProjectTile background='url(/projects/celesteia.jpg)' links={[
+                <a href='//www.celesteia.com'>Visit celesteia.com</a>
+            ]}>
+                <h1>Celesteia</h1>
+                <p>
+                    A 2D sandbox game featuring procedural world generation and resource management systems, presented as my final high school 
+                    project. The method of managing the project has changed the teaching methods of computer science at the school, acting as
+                    an approachable project management method. This project won the school's award for Best Project in the Sciences.
+                </p>
+            </ProjectTile>
+            <ProjectTile background='url(/projects/leafal.jpg)' links={[
+                <a href='//www.leafal.io'>Visit leafal.io</a>
+            ]}>
+                <h1>leafal.io</h1>
+                <p>
+                    Set to be a next-generation hub for games, allowing users to purchase games, play games and interact with other players.
+                </p>
+            </ProjectTile>
+            <ProjectTile background='url(/projects/tosc.jpg)' links={[
+                <a href='//theopensource.company'>Visit theopensource.company</a>
+            ]}>
+                <h1>The Open Source Company</h1>
+                <p>
+                    A collective advocating for libre software and a web built on transparency, started by two of my friends and myself. With
+                    The Open Source Company, I've designed the collective's website, Kards, Playrbase and recently brought leafal.io under its
+                    banner.
+                </p>
+            </ProjectTile>
+        </div>
+
+        <h2>Here's where you can find me.</h2>
+        <div className={styles.socials}>
+            <a href='//github.com/leafeling' target='_blank'>
+                <SiGithub />
+            </a>
+            <a href='//tech.lgbt/@leafeling' target='_blank'>
+                <SiMastodon />
+            </a>
         </div>
     </div>
 

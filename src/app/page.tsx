23eb04ed09ba31fd41258de643a7fbcde330604e
Hazel Hofmann <hazel@theopensource.company>
2024-01-11 20:24:38 +0100

    remove socials and merge github link into stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import styles from '@/styles/Home.module.scss'
 import { Metadata } from 'next';
 import Image from 'next/image';
 import { ProjectTile } from './components/ProjectTile';
-import { SiGithub, SiMastodon } from '@icons-pack/react-simple-icons';
 
 export const metadata: Metadata = {
     title: "Hazel, that's me.",
@@ -63,21 +62,11 @@ const Home = () =>
 
         <h2>{`Here's what I use to get the job done.`}</h2>
         <div className={styles.languages}>
-            <a href="https://github.com/anuraghazra/github-readme-stats" target='_blank'>
+            <a href="//github.com/leafeling" target='_blank'>
                 <img
                     src="https://github-readme-stats.vercel.app/api/top-langs/?username=leafeling&theme=dark&bg_color=0000&disable_animations=true&hide_border=true&hide_title=true&text_bold=true&langs_count=12&layout=compact"
                     alt="Language statistics, unfortunately can't really grab the content for readability."
                 />
-            </a>
-        </div>
-
-        <h2>{`Here's where you can find me.`}</h2>
-        <div className={styles.socials}>
-            <a href='//github.com/leafeling' target='_blank'>
-                <SiGithub />
-            </a>
-            <a href='//tech.lgbt/@leafeling' target='_blank'>
-                <SiMastodon />
             </a>
         </div>
     </div>

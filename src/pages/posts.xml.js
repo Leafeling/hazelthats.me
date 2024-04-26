6dd7dff5fd325b376f2f84b588c5c77ad600da14
Hazel <git@hazelthats.me>
2024-04-26 21:56:08 +0200

    very basic RSS feed

---
+++ src/pages/posts.xml.js
@@ -0,0 +1,13 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+const posts = await pagesGlobToRssItems(import.meta.glob('./posts/*.md'));
+
+export async function GET() {
+  return rss({
+    title: `Hazel, that's me.`,
+    description: `A handy RSS feed of all my blog posts.`,
+    site: `https://hazelthats.me/`,
+    trailingSlash: false,
+    items: posts,
+  });
+}
